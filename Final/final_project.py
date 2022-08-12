353453a3b89e59fe7a742bd328fd08a6b443ef47
Cecilia Laughon <claughon3@hotmail.com>
2022-08-12 16:23:34 -0400

    Upgrades Round 4

--- Final/final_project.py
+++ Final/final_project.py
@@ -22,7 +22,7 @@ def RPC():
             player_win +=1
             print(f"Your opponent chose Scissors, you chose Rock!")
             print(f"Rock CRUSHES Scissors, YOU WIN!!")
-            print("\U0001FAA8")
+            print("\U0001F5FF")
         elif player_choice.title() == "Rock" and comp_choice == "Paper":
             print(f"Your opponent chose Paper, you chose Rock!")
             print(f"Paper SMOTHERS Rock, you LOSE!")
@@ -31,7 +31,7 @@ def RPC():
         elif player_choice.title() == "Scissors" and comp_choice == "Rock":
             print(f"Your opponent chose Rock, you chose Scissors!")
             print(f"Rock CRUSHES Scissors, you LOSE!")
-            print("\U0001FAA8")
+            print("\U0001F5FF")
             comp_win +=1
         elif player_choice.title() == "Scissors" and comp_choice == "Paper":
             print(f"Your opponent chose Paper, you chose Scissors!")
@@ -53,3 +53,22 @@ def RPC():
             print("\U0001F504")
     return player_win        
 RPC()
+
+def prize_wheel():
+    prize_wheel = random.choice(["bean","video game", "money","house"])
+    ticket = RPC()
+    prizes = []
+    while ticket > 0:
+        prize_wheel = random.choice(["tickets","bean","video game", "money","house"])
+        wheel_choice = input(f"You have {ticket} tickets. Would you like to spin?")
+        if wheel_choice == "yes":
+            ticket -= 1
+            prizes.append(prize_wheel)
+            print(prizes)            
+        elif wheel_choice == "no":
+            print(f"Thanks for playing! Here are your prizes! {prizes}")
+            break
+    while ticket == 0:
+        print(f"Here are your prizes! {prizes} Come again!")
+        break
+prize_wheel()
